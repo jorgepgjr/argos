aa03d54b485492bd358f59eea6efb30dfa0642a4
Raphael de Oliveira Costa <raocosta@gmail.com>
2014-05-08 17:53:02 -0300

    Issue#4: Mostrar a instancia que está sempre

---
+++ constants.js
@@ -0,0 +1,56 @@
+var ARGOS = {
+    DESENV_SERVERS:{
+        ABRILSAC:[
+            'JBDEV11',
+            'JBDEV23'
+        ]
+    },
+
+    HOMOLOG_SERVERS:{
+        ABRILSAC:[
+            'JBHOM23',
+            'JBHOM34'
+        ],
+        CLUBE:[
+            'JBHOM04',
+			'JBHOM09'
+        ],
+        ASSINE:[
+            'JBHOM01',
+            'JBHOM02',
+            'JBHOM06',
+            'JBHOM07'
+        ],
+        SERVICOSASS:[
+            'JBHOM05',
+			'JBHOM10'
+        ]
+    },
+
+    PROD_SERVERS:{
+        ABRILSAC:[
+            'JBPRD23',
+            'JBPRD34'
+        ],
+        CLUBE:[
+			'JBPRD04',
+			'JBPRD09'
+        ],
+        ASSINE:[
+            'JBPRD01',
+            'JBPRD02',
+            'JBPRD06',
+            'JBPRD07',
+            'JBPRD19',
+            'JBPRD20'
+        ],
+        SERVICOSASS:[
+            'JBPRD05',
+			'JBPRD10'
+        ]
+    },
+
+};
+
+
+
--- script.js
+++ script.js
@@ -1,39 +1,46 @@
-var instanceNames = new Array();
 
 $(document).ready(function() {
 
 	chrome.windows.getCurrent(function(win) {
-	
-		try {		
+
+		try {
 			chrome.tabs.getAllInWindow(win.id, function(tabs) {
-				$.each(tabs, function(index, tab) {			
-				
+				$.each(tabs, function(index, tab) {
+
 					if (tab.active && tab.url.indexOf('abril') > 0) {
-					
-						doConfigSystem(tab.url);
-						
+
+						var instanceNames = doConfigSystem(tab.url);
+
 						var title = tab.title;
-						
+
 						chrome.cookies.get({"url": tab.url, "name": "JSESSIONID"}, function(cookie) {
-							var currenteInstanceName = cookie.value.split(".")[1]						
-						
+							var currentInstanceName = cookie.value.split(".")[1]
+
 							$('li#title').html(title);
-							
-							$.each(instanceNames, function(index2, instanceName) {
-								htmlInstanceName = '';
-								if (instanceName == currenteInstanceName) {
-									htmlInstanceName = '<li class="active">' + currenteInstanceName + '</li>';
-									
-								} else {
-									htmlInstanceName = '<li class="inactive">' + instanceName + '</li>';
-								}
-								
-								$('li#instanceName ul').append(htmlInstanceName);
-							});
-							
+
+                            instanceFound = false;
+                            if ( instanceNames.length > 0){
+								$.each(instanceNames, function(index2, instanceName) {
+									htmlInstanceName = '';
+
+                                    if (instanceName == currentInstanceName) {
+										htmlInstanceName = '<li class="active">' + currentInstanceName + '</li>';
+                                        instanceFound = true;
+
+									} else {
+										htmlInstanceName = '<li class="inactive">' + instanceName + '</li>';
+									}
+							        $('li#instanceName ul').append(htmlInstanceName);
+
+								});
+							}
+
+                            if ( !instanceFound ){
+                                $('li#instanceName ul').append('<li class="active">' + currentInstanceName + '</li>');
+                            }
 						});
 					}
-				});					
+				});
 			});
         }
         catch(e){
@@ -44,54 +51,36 @@ $(document).ready(function() {
 
 function doConfigSystem(url) {
 
-	// Instâncias do AssineAbril
-	if (url.indexOf('assine.abril') > 0) {
-		if (url.toLowerCase().indexOf('homolog') > 0 || url.toLowerCase().indexOf('jbhom') > 0 ) {
-			instanceNames[0] = 'JBHOM01';
-			instanceNames[1] = 'JBHOM02';
-			instanceNames[2] = 'JBHOM06';
-			instanceNames[3] = 'JBHOM07';
-		} else {	
-			instanceNames[0] = 'JBPRD01';
-			instanceNames[1] = 'JBPRD02';
-			instanceNames[2] = 'JBPRD06';
-			instanceNames[3] = 'JBPRD07';
-			instanceNames[4] = 'JBPRD19';
-			instanceNames[5] = 'JBPRD20';
-		}
-	}
-	
-	// Instâncias do SAC Abril
-	if (url.indexOf('sac.abril') > 0) {
-		if (url.toLowerCase().indexOf('homolog') > 0 || url.toLowerCase().indexOf('jbhom') > 0 ) {
-			instanceNames[0] = 'JBHOM03';
-			instanceNames[1] = 'JBHOM08';
-			instanceNames[2] = 'JBHOM11';
-			instanceNames[3] = 'JBHOM23';
-		} else {	
+    instanceNames = new Array();
 
-		}
-	}
-	
-	// Instâncias do Clube do Assinante
-	if (url.indexOf('clubedoassinante.abril') > 0) {
-		if (url.toLowerCase().indexOf('homolog') > 0 || url.toLowerCase().indexOf('jbhom') > 0 ) {
-			instanceNames[0] = 'JBHOM04';
-			instanceNames[1] = 'JBHOM09';
-		} else {	
-			instanceNames[0] = 'JBPRD04';
-			instanceNames[1] = 'JBPRD09';
-		}
-	}
-	
-	// Instâncias de serviços e TOF
-	if (url.indexOf('servicosass.abril') > 0 || url.indexOf('tof.abril') > 0) {
-		if (url.toLowerCase().indexOf('homolog') > 0 || url.toLowerCase().indexOf('jbhom') > 0 ) {
-			instanceNames[0] = 'JBHOM05';
-			instanceNames[1] = 'JBHOM10';
-			instanceNames[2] = 'JBHOM15';
-			instanceNames[2] = 'JBHOM16';
-		} else {	
-		}
-	}
+    isDev = url.toLowerCase().indexOf('desenv') > 0 || url.toLowerCase().indexOf('jbdev') > 0;
+    isHomolog = url.toLowerCase().indexOf('homolog') > 0 || url.toLowerCase().indexOf('jbhom') > 0;
+
+    environment = (isDev)?1:(isHomolog)?2:3;
+
+    envDef = new Array();
+    switch (environment){
+        case 1:
+            envDef = ARGOS.DESENV_SERVERS;
+            break;
+        case 2:
+            envDef = ARGOS.HOMOLOG_SERVERS;
+            break;
+        case 3:
+            envDef = ARGOS.PROD_SERVERS;
+            break;
+
+    }
+
+	if (url.indexOf('assine.abril') > 0){
+        instanceNames = envDef.ASSINE;
+    } else if (url.indexOf('sac.abril') > 0){
+        instanceNames = envDef.ABRILSAC;
+    } else if (url.indexOf('clubedoassinante.abril') > 0){
+        instanceNames = envDef.CLUBE;
+    } else if (url.indexOf('servicosass.abril') > 0 || url.indexOf('tof.abril') > 0){
+        instanceNames = envDef.SERVICOSASS;
+    }
+
+	return instanceNames;
 }
